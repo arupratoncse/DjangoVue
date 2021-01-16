e61d5397073b5a5fb0baebd898be606d25bed22b
Md Raton Ali <raton@marianaocean.com>
2021-01-16 22:45:16 +0900

    cors setup

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -49,17 +49,17 @@ REST_FRAMEWORK = {
         ('knox.auth.TokenAuthentication',)
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'leadmanager.urls'
